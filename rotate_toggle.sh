26072cac62642359f2dfe570bb7a029c3ec52c75
ShadowKyogre <shadowkyogre@aim.com>
2015-05-05 11:34:52 -0700

    rotate_toggle.sh: Stylus and erasor use the same rotate value, don't try to flip it twice

--- rotate_toggle.sh
+++ rotate_toggle.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-touch_ids=$(xsetwacom --list devices|grep "type: \(STYLUS\|ERASER\)"|grep -Eo "id: [0-9]+"|sed 's/id: //g;s/\\n/ /g')
+touch_ids=$(xsetwacom --list devices|grep "type: STYLUS"|grep -Eo "id: [0-9]+"|sed 's/id: //g;s/\\n/ /g')
 touch_ids=(${touch_ids//:/ })
 tLen=${#touch_ids[@]}
-for ((i=0; i<${tLen}; i++ ));
+
+for device_id in ${touch_ids[@]}
 do
-	if [ "$(xsetwacom --get ${touch_ids[$i]} rotate)" == "half" ];then
-		xsetwacom set ${touch_ids[$i]} rotate none
+	if [ "$(xsetwacom --get ${device_id} rotate)" == "half" ];then
+		xsetwacom set ${device_id} rotate none
 	else
-		xsetwacom set ${touch_ids[$i]} rotate half
+		xsetwacom set ${device_id} rotate half
 	fi
 done
