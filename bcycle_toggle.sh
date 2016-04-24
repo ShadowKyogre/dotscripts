8ade39e7a28dbeea0a5ddcb7de9b2a6b0274eea8
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-04-24 09:56:24 -0700

    Send notification to all attached tmux clients too

--- bcycle_toggle.sh
+++ bcycle_toggle.sh
@@ -13,10 +13,19 @@ fi
 bc_pane="${bc_id}.0"
 tmux send-keys -t "${bc_pane}" Space
 
+notify_all_clients()
+{
+	for client in $(tmux list-clients -F '#{client_tty}');do
+		tmux display-message -c "${client}" "${1}"
+	done
+}
+
 if [  "$(tmux show-window-options -vt "${bc_id}" "${toggle_opt}")" -eq 0 ];then
-	notify-send "Paused timer"
 	tmux set-window-option -t "${bc_id}" "${toggle_opt}" 1
+	notify-send "Paused timer"
+	notify_all_clients "Paused timer"
 else
-	notify-send "Unpaused timer"
 	tmux set-window-option -t "${bc_id}" "${toggle_opt}" 0
+	notify-send "Unpaused timer"
+	notify_all_clients "Paused timer"
 fi
