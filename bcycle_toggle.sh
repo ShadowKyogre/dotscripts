4facb63a4372dbb8893234cbf5df85866121d698
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-04-24 09:40:10 -0700

    Report timer state when script is run

--- bcycle_toggle.sh
+++ bcycle_toggle.sh
@@ -2,6 +2,7 @@
 
 tsession="${1:-dropdown}"
 twindow="${2:-breakcycle}"
+toggle_opt="${3:-@bcycle-paused}"
 
 bc_id="$(tmux list-windows -F '#{window_id}:#W' -t "${tsession}"|grep ":${twindow}$"|cut -d: -f1)"
 
@@ -11,3 +12,11 @@ fi
 
 bc_pane="${bc_id}.0"
 tmux send-keys -t "${bc_pane}" Space
+
+if [  "$(tmux show-window-options -vt "${bc_id}" "${toggle_opt}")" -eq 0 ];then
+	notify-send "Paused timer"
+	tmux set-window-option -t "${bc_id}" "${toggle_opt}" 1
+else
+	notify-send "Unpaused timer"
+	tmux set-window-option -t "${bc_id}" "${toggle_opt}" 0
+fi
