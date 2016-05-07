ba2fbd7b96b9c598e829a2fdc287e31d8c7e70ab
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-05-07 15:35:08 -0700

    Backup script for taking censored screenshots

---
+++ censored_screenshot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+filename="/tmp/$(date +%F-%H%M%S).png"
+sleep "${1:-0}"
+
+if [ "$3" = "yes" ];then
+	maim --showcursor "$filename"
+else
+	maim "$filename"
+fi
+
+if [ "$2" = "yes" ];then
+	convert "$filename" \
+	    -fill '#000f' -draw 'rectangle 174,1044,270,1076' \
+	    -font 'AveriaSerif-Regular' -pointsize 20 \
+	    -fill red -annotate +184+1067 'Sigils' \
+	    "$filename"
+fi
+notify-send "Screenshot saved" "$filename"
