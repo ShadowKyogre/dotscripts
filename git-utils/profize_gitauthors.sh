be7eab339122cf8d236e23176c1fb11a5b45e597
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-05-16 13:57:33 -0700

    Make profize_gitauthors reusable w/dif params
    Use as such instead of copypastaing the contents:
    
    git filter-branch --env-filter \
        'eval $(profize_gitauthors.sh \
                "old@email.com" \
                "new name" \
                "new@email.com")'
    
    printf '%q' is love

--- git-utils/profize_gitauthors.sh
+++ git-utils/profize_gitauthors.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-git filter-branch --env-filter '
-
 an="$GIT_AUTHOR_NAME"
 am="$GIT_AUTHOR_EMAIL"
 cn="$GIT_COMMITTER_NAME"
 cm="$GIT_COMMITTER_EMAIL"
 
-if [ "$GIT_COMMITTER_EMAIL" = "shadowkyogre@aim.com" ]
-then
-    cn="Something"
-    cm="Something@something.com"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "shadowkyogre@aim.com" ]
-then
-    an="Something"
-    am="Something@something.com"
+if [ "$#" -ge 3 ];then
+    if [ "$GIT_COMMITTER_EMAIL" = "$1" ]
+    then
+        cn="$2"
+        cm="$3"
+    fi
+    if [ "$GIT_AUTHOR_EMAIL" = "$1" ]
+    then
+        an="$2"
+        am="$3"
+    fi
 fi
 
-export GIT_AUTHOR_NAME="$an"
-export GIT_AUTHOR_EMAIL="$am"
-export GIT_COMMITTER_NAME="$cn"
-export GIT_COMMITTER_EMAIL="$cm"
-'
+echo "export GIT_AUTHOR_NAME=$(printf '%q' "$an")"
+echo "export GIT_AUTHOR_EMAIL=$(printf '%q' "$am")"
+echo "export GIT_COMMITTER_NAME=$(printf '%q' "$cn")"
+echo "export GIT_COMMITTER_EMAIL=$(printf '%q' "$cm")"
