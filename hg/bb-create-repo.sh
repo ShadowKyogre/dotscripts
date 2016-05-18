82c2850c1835f7e5e93652e72f5d92023924b2fc
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-05-18 13:48:21 -0700

    Bug fixes for bb-create-repo.sh
    - OWNER should default to BB_USER
    - Output data for a repo if it already exists
    - Make filter flexible depending on BB_FILTER env variable

--- hg/bb-create-repo.sh
+++ hg/bb-create-repo.sh
@@ -9,12 +9,21 @@ BB_LOGIN="$(sed -n 1p "$HOME/.config/.bb_auth")"
 BB_PW="$(sed -n 2p "$HOME/.config/.bb_auth")"
 BB_USER="$(sed -n 3p "$HOME/.config/.bb_auth")"
 
+DEF_BB_FILTER=".links.clone[1].href"
+BB_FILTER="${BB_FILTER:-${DEF_BB_FILTER}}"
+
 for repo in $@;do
-	OWNER="${BB_OWNER:-${BB_LOGIN}}"
-	curl --user "${BB_LOGIN}:${BB_PW}" \
+	OWNER="${BB_OWNER:-${BB_USER}}"
+	DATA="$(curl --user "${BB_LOGIN}:${BB_PW}" \
 		"https://api.bitbucket.org/2.0/repositories/${OWNER}/${repo}" \
 		--data name="$repo" \
 		--data fork_policy=no_public_forks \
 		--data is_private='true' \
-		--data scm=hg|jq '.links.clone[1].href' -r
+		--data scm=hg|jq "${BB_FILTER}" -r)"
+	if [ "$DATA" = "null" ];then
+		DATA="$(curl -X GET --user "${BB_LOGIN}:${BB_PW}" \
+		"https://api.bitbucket.org/2.0/repositories/${OWNER}/${repo}"| \
+		jq "${BB_FILTER}" -r)"
+	fi
+	echo "$DATA"
 done
