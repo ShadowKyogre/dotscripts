0b5bd6662169f4502e122c458f899957cfc47ae3
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-11-28 11:10:48 -0800

    Add point buy script

---
+++ misc/dnd-point-buy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from collections import OrderedDict as od
+from argparse import ArgumentParser
+
+def pb_to_stat(val):
+    if val <= 5:
+        return val
+    elif val == 7:
+        return 6
+    elif val == 9:
+        return 7
+    else:
+        raise ValueError("Invalid point buy cost: {0}".format(val))
+
+aparser = ArgumentParser()
+aparser.add_argument('cost', type=int, default=0, nargs='?')
+
+args = aparser.parse_args()
+print(pb_to_stat(args.cost))
