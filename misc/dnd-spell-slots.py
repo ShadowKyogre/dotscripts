ce3e605e46c159473d8cc08f5b0bb42da51d7f45
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-11-28 13:31:52 -0800

    Add support for warlock levels

--- misc/dnd-spell-slots.py
+++ misc/dnd-spell-slots.py
@@ -2,6 +2,36 @@
 
 from argparse import ArgumentParser
 
+def cwarlock_slots(level):
+    slot_level = round(min(level, 10)/2 + .1)
+
+    if level == 1:
+        slot_num = 1
+    elif level <= 4:
+        slot_num = 2
+    elif level <= 10:
+        slot_num = 3
+    elif level <= 16:
+        slot_num = 4
+    elif level >= 17:
+        slot_num = 5
+
+    return slot_level, slot_num
+
+def warlock_slots(level):
+    slot_level = round(min(level, 10)/2 + .1)
+
+    if level == 1:
+        slot_num = 1
+    elif level <= 10:
+        slot_num = 2
+    elif level <= 16:
+        slot_num = 3
+    elif level >= 17:
+        slot_num = 4
+
+    return slot_level, slot_num
+
 def caster_slots(level):
     slots = [2, 0, 0, 0, 0, 0, 0, 0, 0]
     slots[0] += min(level, 3) - 1
@@ -34,6 +64,8 @@ def caster_slots(level):
 
 aparser = ArgumentParser()
 aparser.add_argument('-l', '--level', help='Print level only', action='store_true')
+aparser.add_argument('--warlock', help='warlock levels', type=int, default=0)
+aparser.add_argument('--cwarlock', help='cwarlock levels', type=int, default=0)
 aparser.add_argument('full_caster', type=int, default=0)
 aparser.add_argument('half_caster', type=int, default=0, nargs='?')
 aparser.add_argument('third_caster', type=int, default=0, nargs='?')
@@ -60,5 +92,25 @@ for i in range(lv20s):
 if others > 0:
     slots = [x + y for x, y in zip(slots, caster_slots(others))]
 
-print(slots)
 
+lv20s_w, others_w = divmod(args.warlock, 20)
+
+for i in range(lv20s_w):
+    lvl, num = warlock_slots(20)
+    slots[lvl-1] += num
+
+if others_w > 0:
+    lvl, num = warlock_slots(others_w)
+    slots[lvl-1] += num
+
+lv20s_cw, others_cw = divmod(args.cwarlock, 20)
+
+for i in range(lv20s_cw):
+    lvl, num = cwarlock_slots(20)
+    slots[lvl-1] += num
+
+if others_cw > 0:
+    lvl, num = cwarlock_slots(others_cw)
+    slots[lvl-1] += num
+
+print(slots)
