35e439b5ed5db83fb273e8b00796d94855dd3085
ShadowKyogre <shadowkyogre.public@gmail.com>
2020-11-19 22:44:52 -0800

    git-utils: Add git-sw-edit.sh
    Basically a dumb way to stopwatch freeform editing logs.

---
+++ git-utils/git-sw-edit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+TIME1=$(date "+%Y-%m-%d %H:%M:%S")
+
+"${EDITOR:-vim}" "$@"
+EXIT_CODE="$?"
+TIME2=$(date "+%Y-%m-%d %H:%M:%S")
+
+if [["${EXIT_CODE}" == 0 ]]; then
+	git-timelog.sh "${TIME1}" "${TIME2}"
+fi
