d35f88ce31132a5bf44d767e53cbad1455972088
ShadowKyogre <shadowkyogre.public@gmail.com>
2020-11-19 22:24:22 -0800

    timers: Separate log handler from main loop
    One should be able to have termdoro manage marking timestamps and diffs
    with a git or other VCS script too.

--- timers/termdoro.sh
+++ timers/termdoro.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 INTERVAL="${1:-0}"
-LOG_FILE=$(mktemp -t "termdoro-intervals.XXXXXXXXX" -q)
 FONT="3x5"
+LOG_HANDLER="${LOG_HANDLER:-termdoro_log}"
+
+function termdoro_log() {
+	if [[ -z "${LOG_FILE}" ]]; then
+		LOG_FILE=$(mktemp -t "termdoro-intervals.XXXXXXXXX" -q)
+	fi
+	printf "${INTERVAL}\t${TIME1}\t${TIME2}\n" >> ${LOG_FILE}
+}
 
 while true; do
 	TIME1="$(date "+%Y-%m-%d %H:%M:%S")"
@@ -12,7 +19,7 @@ while true; do
 	if [[ "${EXIT_CODE}" != 0 ]]; then
 		exit
 	fi
-	printf "${INTERVAL}\t${TIME1}\t${TIME2}\n" >> ${LOG_FILE}
+	${LOG_HANDLER} ${TIME1} ${TIME2}
 	if [[ "${INTERVAL}" == "3" ]];then
 		termdown -a "30m" -f "${FONT}" -T "Long Break"
 	else
