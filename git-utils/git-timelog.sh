75ac13994d982c600bd67812e2ad1ff219bda578
ShadowKyogre <shadowkyogre.public@gmail.com>
2020-11-28 21:22:28 -0800

    git-utils: Put timelog data in git timelog notes
    Make use of the git notes feature to describe things not related to the
    main body of the text recorded.

--- git-utils/git-timelog.sh
+++ git-utils/git-timelog.sh
@@ -10,7 +10,12 @@ if [[ -z "${2}" ]];then
 	exit 1
 fi
 
-message="$(printf "short: summary\n\n${1}\t${2}\n\nYour remarks for later here.\n")"
+message="$(printf "short: summary\n\nYour remarks for later here.\n")"
+
+# Make sure you configure git properly!
+# https://stackoverflow.com/questions/2683248/can-i-add-metadata-to-git-commits-or-can-i-hide-some-tags-in-gitk
+# This setup assumes that you're namespacing your notes right in git!
 
 git add .
 git commit -m "${message}"
+git notes --ref=timelog add -m "Write Burst Start: ${1}" -m "Write Burst End: ${2}"
