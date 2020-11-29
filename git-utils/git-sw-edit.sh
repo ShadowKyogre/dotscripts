0d361ce7d2b9870546ce8d44c5dec5b535758977
ShadowKyogre <shadowkyogre.public@gmail.com>
2020-11-28 21:21:42 -0800

    git-utils: git-sw-edit checks if in git repo
    This is to avoid calling the timelog script when not needed

--- git-utils/git-sw-edit.sh
+++ git-utils/git-sw-edit.sh
@@ -7,5 +7,9 @@ EXIT_CODE="$?"
 TIME2=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [[ "${EXIT_CODE}" == 0 ]]; then
-	git-timelog.sh "${TIME1}" "${TIME2}"
+	printf "${TIME1}\t${TIME2}\n" 1>&2
+	if git rev-parse --git-dir > /dev/null 2>&1; then
+		printf "Saving write burst to git repo\n" 1>&2
+		git-timelog.sh "${TIME1}" "${TIME2}"
+	fi
 fi
